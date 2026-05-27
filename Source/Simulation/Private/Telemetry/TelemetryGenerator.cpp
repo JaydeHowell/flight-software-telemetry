bde7983c953bac5aa8f1f6809461f5eb9112387a
Jayde Howell <jhowel62@wgu.edu>
2026-05-27 07:16:10 -0700

    UDP broadcast on port 8080

---
+++ Source/Simulation/Private/Telemetry/TelemetryGenerator.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Simulation/Public/Telemetry/TelemetryGenerator.h"
+
+#include "Serialization/ArrayWriter.h"
+
+
+// Sets default values
+ATelemetryGenerator::ATelemetryGenerator()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATelemetryGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FIPv4Address IPAddress = FIPv4Address::InternalLoopback;
+	RemoteEndpoint = FIPv4Endpoint(IPAddress, 8080);
+	
+	UdpSocket = FUdpSocketBuilder(TEXT("TelemetrySocket"))
+		.AsNonBlocking()
+		.AsReusable()
+		.WithBroadcast();
+	
+}
+
+// Called every frame
+void ATelemetryGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATelemetryGenerator::SendTelemetryPacket()
+{
+	if (!UdpSocket) return;
+	
+	float ChamberPressure = 101.3f;
+	float BusVoltage = 28.0f;
+	
+	FArrayWriter Writer;
+	Writer << ChamberPressure;
+	Writer << BusVoltage;
+	
+	int32 BytesSent = 0;
+	UdpSocket->SendTo(Writer.GetData(), Writer.Num(), BytesSent, *RemoteEndpoint.ToInternetAddr());
+}
+
---
+++ Source/Simulation/Public/Telemetry/TelemetryGenerator.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Interfaces/IPv4/IPv4Endpoint.h"
+#include "Interfaces/IPv4/IPv4Address.h"
+#include "Common/UdpSocketBuilder.h"
+#include "SocketSubsystem.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TelemetryGenerator.generated.h"
+
+
+UCLASS()
+class SIMULATION_API ATelemetryGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATelemetryGenerator();
+
+protected:
+	FSocket* UdpSocket;
+	FIPv4Endpoint RemoteEndpoint;
+	
+	void SendTelemetryPacket();
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
